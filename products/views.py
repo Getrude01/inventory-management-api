09594ad07d61cb23f8d9d6a52201adc7d70774e8
Getrude <getrudengure6@gmail.com>
2025-12-26 21:51:47 +0300

    Changed the settings error

--- products/views.py
+++ products/views.py
@@ -1,10 +1,10 @@
 from rest_framework.generics import ListCreateAPIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Product
 from .serializers import ProductSerializer
 
 class ProductListCreateView(ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = []
+    permission_classes = [AllowAny]
     
