c004ad89d02bfa731ad2fb7eb3a5e8366184eba9
Getrude <getrudengure6@gmail.com>
2025-12-30 15:23:58 +0300

    Fix ALLOWED_HOSTS for deployment

--- inventory_api/settings.py
+++ inventory_api/settings.py
@@ -31,11 +31,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    'inventory-management-api-cjfm.onrender.com',
-]
+ALLOWED_HOSTS = ["*"]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
