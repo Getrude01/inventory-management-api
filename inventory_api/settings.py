4b66a34b9c90d6ae87f5665bbd2b600307f3587f
Getrude <getrudengure6@gmail.com>
2025-12-30 15:48:13 +0300

    Fix DEBUG for deployment

--- inventory_api/settings.py
+++ inventory_api/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "unsafe-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = os.environ.get("DEBUG", "False") == "False"
 
 ALLOWED_HOSTS = ["*"]
 
